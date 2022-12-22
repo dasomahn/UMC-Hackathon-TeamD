026ff707c7ca823a280bc8f854d89c46988c93cd
Dasom Ahn <lizzy0521@naver.com>
2022-12-23 01:18:09 +0900

    #1 update: .gitignore

---
+++ Server/SpringBoot_Template/src/main/java/com/example/demo/src/home/HomeService.java
@@ -0,0 +1,10 @@
+package com.example.demo.src.home;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class HomeService {
+
+}
